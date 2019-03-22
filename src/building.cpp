985479acb26df4df996367f68879f080467f04cb
Chris May <may5@purdue.edu>
2019-03-22 14:38:25 -0400

    Reduce saturation of brightened facades
    They were really blue or red sometimes. This helps that some.

--- src/building.cpp
+++ src/building.cpp
@@ -620,6 +620,9 @@ void Building::synthFacadeGeometry(fs::path outputDir, map<size_t, fs::path> fac
 		float brt = glm::dot(fp.bg_color, glm::vec3(1.0f)) / 3.0f;
 		if (brt < 0.3f) {
 			fp.bg_color *= 0.5f / brt;
+			// Make closer to average (reduce saturation)
+			float avg = glm::dot(fp.bg_color, glm::vec3(1.0f)) / 3.0f;
+			fp.bg_color += (glm::vec3(avg) - fp.bg_color) * 0.4f;
 			fp.bg_color = glm::clamp(fp.bg_color, 0.0f, 1.0f);
 		}
 		// Set initial window color to 60% of background color
